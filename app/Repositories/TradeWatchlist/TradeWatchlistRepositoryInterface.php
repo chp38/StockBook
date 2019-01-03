6c816f2f4a8d73c2637bfe5580570fcdb3f17e0a
Charles Palmer <charles.wpalmer@outlook.com>
2019-01-03 21:20:55 +0000

    Added watchlist service and repo

---
+++ app/Repositories/TradeWatchlist/TradeWatchlistRepositoryInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: charlespalmer
+ * Date: 03/01/2019
+ * Time: 21:20
+ */
+
+namespace App\Repositories\TradeWatchlist;
+
+
+class TradeWatchlistRepositoryInterface
+{
+
+}
---
+++ app/Services/WatchlistService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: charlespalmer
+ * Date: 03/01/2019
+ * Time: 21:19
+ */
+
+namespace App\Services;
+
+
+class WatchlistService
+{
+
+}
