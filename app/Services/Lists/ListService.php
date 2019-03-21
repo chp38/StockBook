ed7fea906a963edc7dc5d7cb2443e96cfdeb8345
Charles Palmer <charles.palmer@enta.net>
2019-03-21 18:05:23 +0000

    Include trade details repository

--- app/Services/Lists/ListService.php
+++ app/Services/Lists/ListService.php
@@ -8,7 +8,10 @@
 
 namespace App\Services\Lists;
 
+use App\Model\TradeWatchlist;
 use App\Repositories\RepositoryInterface;
+use App\Repositories\TradeDetails\TradeDetailsRepository;
+use Illuminate\Support\Facades\App;
 
 class ListService
 {
@@ -18,13 +21,20 @@ class ListService
     protected $repository;
 
     /**
+     * @var TradeDetailsRepository
+     */
+    protected $tradeDetails;
+
+    /**
      * ListService constructor.
      *
-     * @param RepositoryInterface $repository
+     * @param RepositoryInterface      $repository
+     * @param RepositoryInterface|null $tradeDetails
      */
-    public function __construct(RepositoryInterface $repository)
+    public function __construct(RepositoryInterface $repository = null, RepositoryInterface $tradeDetails = null)
     {
         $this->repository = $repository;
+        $this->tradeDetails = $tradeDetails;
     }
 
     /**
@@ -34,5 +44,29 @@ class ListService
     public function getAll()
     {
         return $this->repository->all();
+    }
+
+    /**
+     * Add a currency pair to a list from the homepage
+     *
+     * @param String $listType
+     * @param Int    $pairId
+     * @return TradeWatchlist
+     */
+    public function addFromHomepage($listType, $pairId)
+    {
+        switch($listType) {
+            case 'watchlist':
+                $service = app()->make('\App\Services\Lists\WatchlistService');
+                break;
+            case 'trades':
+                $service = app()->make('\App\Services\Lists\ActiveListService');
+                break;
+            default:
+                $service = app()->make('\App\Services\Lists\WatchlistService');
+                break;
+        }
+
+        return $service->addCurrencyPair($pairId);
     }
 }
--- app/Services/Lists/WatchlistService.php
+++ app/Services/Lists/WatchlistService.php
@@ -8,19 +8,22 @@
 
  namespace App\Services\Lists;
 
+use App\Repositories\TradeDetails\TradeDetailsRepository;
 use App\Repositories\TradeWatchlist\TradeWatchlistRepository;
+use Illuminate\Support\Facades\Auth;
 
 class WatchlistService extends ListService
 {
     /**
      * WatchlistService constructor.
+     *
      * @param TradeWatchlistRepository $repo
+     * @param TradeDetailsRepository   $detailsRepo
      */
-    public function __construct(TradeWatchlistRepository $repo)
+    public function __construct(TradeWatchlistRepository $repo, TradeDetailsRepository $detailsRepo)
     {
-        parent::__construct($repo);
+        parent::__construct($repo, $detailsRepo);
 
-        $this->repository = $repo;
     }
 
     /**
@@ -31,5 +34,26 @@ class WatchlistService extends ListService
     public function getWatchlistItem($id)
     {
         return $this->repository->find($id);
+    }
+
+    /**
+     * Add currency pair to Watchlist and trade detail
+     *
+     * @param $pairId
+     * @return mixed
+     */
+    public function addCurrencyPair($pairId)
+    {
+        $detail = $this->tradeDetails->create([
+            'currency_pair_id' => (int)$pairId,
+            'user_id' => Auth::user()->id,
+            'entry_price' => 1.20
+        ]);
+
+        $watchlist = $this->repository->create([
+            'trade_details_id' => $detail->id
+        ]);
+
+        return $watchlist;
     }
 }
