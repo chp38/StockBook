e8689c35d169acdb8385d4931513857a815dca6f
Charles Palmer <charles.palmer@enta.net>
2019-03-21 18:23:26 +0000

    Add to current trades list

--- app/Model/CurrentTrade.php
+++ app/Model/CurrentTrade.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class CurrentTrade extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'trade_details_id'
+    ];
+
+    /**
+     * @var string $table
+     */
+    protected $table = 'current_trades';
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function detail() {
+        return $this->belongsTo('App\Model\TradeDetail', 'trade_details_id');
+    }
 }
--- app/Services/Lists/ActiveListService.php
+++ app/Services/Lists/ActiveListService.php
@@ -9,16 +9,42 @@
 namespace App\Services\Lists;
 
 use App\Repositories\CurrentTrades\CurrentTradesRepository;
+use App\Repositories\TradeDetails\TradeDetailsRepository;
+use Illuminate\Support\Facades\Auth;
 
 class ActiveListService extends ListService
 {
     /**
      * WatchlistService constructor.
+     *
      * @param CurrentTradesRepository $repo
+     * @param TradeDetailsRepository  $tradeDetails
      */
-    public function __construct(CurrentTradesRepository $repo)
+    public function __construct(CurrentTradesRepository $repo, TradeDetailsRepository $tradeDetails)
     {
-        parent::__construct($repo);
+        parent::__construct($repo, $tradeDetails);
 
         $this->repository = $repo;
-    }}
+    }
+
+    /**
+     * Add currency pair to Watchlist and trade detail
+     *
+     * @param $pairId
+     * @return mixed
+     */
+    public function addCurrencyPair($pairId)
+    {
+        $detail = $this->tradeDetails->create([
+            'currency_pair_id' => (int)$pairId,
+            'user_id' => Auth::user()->id,
+            'entry_price' => 1.20
+        ]);
+
+        $watchlist = $this->repository->create([
+            'trade_details_id' => $detail->id
+        ]);
+
+        return $watchlist;
+    }
+}
