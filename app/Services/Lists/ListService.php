8a40c168818fec94420fd0e8ce71f29da7cfc412
Charles Palmer <charles.palmer@enta.net>
2019-03-21 14:36:34 +0000

    RecentListService and hierarchy ListService

---
+++ app/Services/Lists/ListService.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: charlespalmer
+ * Date: 2019-03-21
+ * Time: 14:22
+ */
+
+namespace App\Services\Lists;
+
+use App\Repositories\RepositoryInterface;
+
+class ListService
+{
+    /**
+     * @var RepositoryInterface
+     */
+    protected $repository;
+
+    /**
+     * ListService constructor.
+     *
+     * @param RepositoryInterface $repository
+     */
+    public function __construct(RepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+}
