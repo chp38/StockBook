f6f5494115856cb1a8c7482007ca8129b03bfa90
Charles Palmer <charlespalmer@Charless-MacBook-Pro.local>
2020-12-31 18:20:30 +0000

    Testing deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,4 @@ docker-compose build
 docker-compose up -d
 
 sleep 10
-
-cp ./env.example .env
-php artisan key:generate
-composer install
-
 docker exec -it stockbook_app php artisan migrate:fresh --seed
