fda2f4ce535dc62718886361f3424e76b886339a
Charles Palmer <charles.palmer99967@gmail.com>
2021-04-30 10:01:59 +0100

    Correct heading

--- resources/views/tokens/index.blade.php
+++ resources/views/tokens/index.blade.php
@@ -36,7 +36,7 @@
     <div class="card mb-4">
                             <div class="card-header">
                                 <i class="fas fa-table mr-1"></i>
-                                DataTable Example
+                                Tokens
                             </div>
                             <div class="card-body">
                                 <div class="table-responsive">
